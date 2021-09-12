90139fa8133339f751bb3745577b2c52282f53c8
Jere Länsipii <jerelans91@gmail.com>
2021-09-12 20:46:11 +0300

    reset project

--- src/Kuva/Kuva.js
+++ src/Kuva/Kuva.js
@@ -7,14 +7,9 @@ import ImageUploading from "react-images-uploading";
 function Kuva(props) {
 
   const [images, setImages] = React.useState([]);
-  const [nappula, setNappula] = React.useState("naytaUpload");
-  const maxNumber = 1;
 
   const onChange = (imageList) => {
-    
     setImages(imageList);
-    setNappula("piilotaUpload")
-
   };
 
   return (
@@ -25,34 +20,21 @@ function Kuva(props) {
 
       <Button variant="outlined" type="submit" onClick={props.onClick}> {"<<"} Palaa etusivulle</Button>
 
-      <ImageUploading multiple value={images} onChange={onChange} maxNumber={maxNumber} dataURLKey="data_url">
-      {({ imageList, onImageUpload, onImageUpdate, isDragging, dragProps }) => (
-      <>
+      <ImageUploading value={images} onChange={onChange} dataURLKey="data_url">
+        {({ imageList, onImageUpload, isDragging, dragProps, }) => (
+
           <div>
-
-            {/* Upload kuva ensimmäistä kertaa */}
-            <button style={isDragging ? { color: 'red' } : undefined} onClick={onImageUpload} {...dragProps}
-            style={{ 
-              display: nappula === "naytaUpload" ? "block" : "none" // Piilota ekan uploadin jälkeen
-              }}> Lisää kuva... </button>
-
+            <button style={isDragging ? { color: 'red' } : undefined} onClick={onImageUpload} {...dragProps}>
+              Click or Drop here
+            </button>
             &nbsp;
-
             {imageList.map((image, index) => (
               <div key={index}>
-                <img src={image['data_url']} alt="" width="150" />
-                <div>
-                  <button onClick={() => onImageUpdate(index)}>Lataa uusi kuva...</button> {/* Näytä ekan uploadin jälkeen */}
-                </div>
+                <img src={image['data_url']} alt="" width="100" />
               </div>
             ))}
-
           </div>
-                      
-          <EfektiLista lista={imageList}/>
-
-      </>
-      )}
+        )}
       </ImageUploading>
 
     </div>
@@ -61,26 +43,6 @@ function Kuva(props) {
   
 }
 
-
-const EfektiLista = (props) => {
-
-
-  return (
-  <div className="efektiLista">
-    {props.lista.map((image, index) => (
-      <div key={index}>
-        <img src={image['data_url']} alt="" width="150" />
-        <img src={image['data_url']} alt="" width="150" />
-        <img src={image['data_url']} alt="" width="150" />
-        <img src={image['data_url']} alt="" width="150" />
-        <img src={image['data_url']} alt="" width="150" />
-        
-      </div>
-      
-    ))}
-  </div>
-  )
-}
 
 
 

63552ccf5ea3a28557ab948d71be7fb81e1894a8
Jere Länsipii <jerelans91@gmail.com>
2021-10-10 01:04:52 +0300

    gym update

--- src/Gymsport/Gymsport.js
+++ src/Gymsport/Gymsport.js
@@ -16,9 +16,12 @@ const liikkeet = [
   }
 ]
 
+const valinnat = []
+
 function Gymsport(props) {
 
   const [category, asetaCategory] = React.useState("Liikkeet")
+  const [valinnatLista, asetaValinnatLista] = React.useState([])
 
   const filterLiikkeet = liikkeet.filter(function(liike) {
     if (category === "Liikkeet") {
@@ -27,6 +30,14 @@ function Gymsport(props) {
       return liike.target.includes(category);
     }
   });
+
+  const handleValinta = function(item) {
+
+    valinnat.push(item.title)
+
+    asetaValinnatLista([...valinnat])
+
+  }
 
   return (
     
@@ -40,15 +51,27 @@ function Gymsport(props) {
       
       <div className="gymsport_flex">
 
-          <div className="gymsport_category">
-            <button className="gymsport_nappi" onClick={() => asetaCategory("Liikkeet")}> Kaikki liikkeet </button>
-            <button className="gymsport_nappi" onClick={() => asetaCategory("Bicep")}> Bicep </button>
-            <button className="gymsport_nappi" onClick={() => asetaCategory("Chest")}> Chest </button>
-          </div>
+        <div className="gymsport_category">
+          <button className="gymsport_nappi" onClick={() => asetaCategory("Liikkeet")}> Kaikki liikkeet </button>
+          <button className="gymsport_nappi" onClick={() => asetaCategory("Bicep")}> Bicep </button>
+          <button className="gymsport_nappi" onClick={() => asetaCategory("Chest")}> Chest </button>
+        </div>
 
-          <div className="gymsport_list">
-            <GymLista gymlista={filterLiikkeet} />
-          </div>
+        <div className="gymsport_list">
+          <GymLista gymlista={filterLiikkeet} siirraValinta={handleValinta} />
+        </div>
+
+      </div>
+
+      <div className="gymsport_flex2">
+
+        <div className="gymsport_program">
+
+        </div>
+
+        <div className="gymsport_selection">
+          <ValintaLista valinnat={valinnatLista} />
+        </div>
 
       </div>
 
@@ -62,13 +85,23 @@ const GymLista = (props) => {
 
   return (
     props.gymlista.map(item => (
+      <div>
+        <h2>{item.title}</h2>
+        <button onClick={() => props.siirraValinta(item)}> Siirrä liikelistaan </button>
+      </div>
+  )))
 
-     <div>
-       <h2>{item.title}</h2>
-     </div>
-
-    )))
 }
 
+const ValintaLista = (props) => {
+
+  return (
+    props.valinnat.map(item => (
+      <div>
+        <h1>{item}</h1>
+      </div>
+    )))
+
+}
 
 export default Gymsport;

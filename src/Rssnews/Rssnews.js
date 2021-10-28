9efa89bb3380e1c464f5086f0800aae961a1d479
Jere Länsipii <jerelans91@gmail.com>
2021-10-28 15:44:34 +0300

    rss update

--- src/Rssnews/Rssnews.js
+++ src/Rssnews/Rssnews.js
@@ -8,25 +8,36 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 function Rssnews(props) {
 
   const [data, setData] = React.useState(null);
-  const [showArticle, asetaShowArticle] = React.useState(false)
-  const [setArticle, asetaSetArticle] = React.useState()
+  const [showArticle, setShowArticle] = React.useState(false)
+
+  // objekti useState
+  const [parseArticle, setParseArticle] = React.useState(
+    {
+      parseotsikko: null,
+      parseartikkeli: null,
+      parselinkki: null  
+    })
+
 
   React.useEffect(() => {
 
-    setTimeout(() => {
-      fetch("https://react-projects-servers.herokuapp.com/rss-server") // NodeJS servu, jossa RSS-feedit käsitellään
-        .then((res) => res.json())
-        .then((data) => setData(data));
-    }, 1000) // näytetään ainakin sekunti hieno loading screen :D
+    /*fetch("https://react-projects-servers.herokuapp.com/rss-server")*/
+
+    fetch("http://localhost:3001/rss-server") // NodeJS servu, jossa RSS-feedit käsitellään
+      .then((res) => res.json())
+      .then((data) => setData(data));
 
   }, []);
 
   const AvaaUutinen = (item) => {
 
-    console.log(typeof item.artikkeli)
+    setShowArticle(true)
 
-    asetaShowArticle(true) 
-    asetaSetArticle(ReactHtmlParser(item.artikkeli)) 
+    setParseArticle({
+      parseotsikko: item.otsikko,
+      parseartikkeli: ReactHtmlParser(item.artikkeli),
+      parselinkki: item.linkki  
+    })
 
   }
 
@@ -38,8 +49,14 @@ function Rssnews(props) {
       
       <Button variant="outlined" type="submit" onClick={props.onClick}> {"<<"} Palaa etusivulle</Button>
 
-        <div className="rssArtikkeli" style={{ display: showArticle ? 'block' : 'none '}}>
-          <p>{setArticle}</p>
+        <div className="rssArtikkeli" style={{ display: showArticle ? 'block' : 'none' }}>
+          <div className="rssArtikkeliContent">
+            <button onClick={() => setShowArticle(false)}>Sulje ikkuna</button>
+
+            <h1>{parseArticle.parseotsikko}</h1>
+            <p>{parseArticle.parseartikkeli}</p>
+            {parseArticle.parselinkki === undefined ? null : <a href={parseArticle.parselinkki} target="_blank">Avaa uutinen</a> }
+          </div>
         </div>
 
         <div className="rssUutiset">
@@ -52,18 +69,27 @@ function Rssnews(props) {
   
 }
 
-
-
 const ListaaData = (props) => {
+
+  const [rssFilter, setRssFilter] = React.useState('Yle')
+
+  const filterData = props.list.filter(uutinen => {
+    return uutinen.sivu.includes(rssFilter)
+  })
 
   return (
 		<div>
-			{props.list.map((item) => (      
+
+      <button onClick={() => setRssFilter('Yle')}>YLE</button>
+      <button onClick={() => setRssFilter('Iltalehti')}>ILTALEHTI</button>
+
+			{filterData.map((item) => (      
         <div className="rssUutinen">
           <h1>{item.otsikko}</h1>
           <button onClick={() => props.uutinenNappi(item)}> Avaa uutinen </button>
         </div>
 			))}
+
 		</div>
   );
 
